b4aa1f256f7717cc8878df504d30aeb33efa4ae4
erycpc <ericmbithiofficial@gmail.com>
2026-07-12 14:19:55 +0300

    feat: setup React frontend with routing and axios API service

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/login" element={<div>Login</div>} />
+        <Route path="/register" element={<div>Register</div>} />
+        <Route path="/jobs" element={<div>Jobs</div>} />
+        <Route path="/jobs/:id" element={<div>Job Detail</div>} />
+        <Route path="/dashboard" element={<div>Dashboard</div>} />
+      </Routes>
+    </Router>
+  )
+}
+export default App
